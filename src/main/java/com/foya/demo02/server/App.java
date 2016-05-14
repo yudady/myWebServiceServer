887e9270037eda00f66748082b0651ea6dd93385
yudady <yu_dady@yahoo.com.tw>
2016-05-14 18:48:12 +0800

    demo02

--- src/main/java/com/foya/demo02/server/App.java
+++ src/main/java/com/foya/demo02/server/App.java
@@ -1,13 +1,13 @@
-package com.foya.demo01.server;
+package com.foya.demo02.server;
 
 import javax.xml.ws.Endpoint;
 
 public class App {
 
 	public static void main(String[] args) {
-		String address = "http://localhost:8888/demo01";
+		String address = "http://localhost:8888/demo02";
 		Endpoint.publish(address, new MyServiceImpl());
-		System.out.println("Endpoint.publish demo01");
+		System.out.println("Endpoint.publish demo02");
 	}
 
 }
